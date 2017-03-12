0d2b73c6e12cc1b9a756f02554f2c7a6b9779110
Selin Fildiş <selinfildis@gmail.com>
2017-03-12 17:41:34 +0300

    Comments added

--- tlib.c
+++ tlib.c
@@ -11,20 +11,24 @@
 typedef struct node{
     node* prev;
     node* next;
+    // pointer for stub
     int tid;
 }
 
 int tlib_init (void)
 {
+  // queue init edilecek
+  //stack ile ilgili bişiyler?????
+  //( queuedan sıradan threadler exec edilcek)
     return (TLIB_ERROR);
 }
 
 /* implementation of stub is already given below */
 void stub (void (*tstartf)(void *), void *arg)
 {
-    
+
 	tstartf (arg); /* calling thread start function to execute */
-    /* 
+    /*
         We are done with executing the application specified thread start
         function. Hence we can now terminate the thread
     */
@@ -35,18 +39,33 @@ void stub (void (*tstartf)(void *), void *arg)
 
 int tlib_create_thread(void (*func)(void *), void *param)
 {
+  /*
+      memory yoksa TLIB_NOMEMORY
+      başka bi sebepten yaraılamazsa FAILURE
+      yaratılınca SUCCESS
+      stub stacke pushlancak,
+      bi tane thread Idsi generate edilecek param kullanılabilir
+      thread Id queue'ya atılcak
+      threadId döndürülecek.
+
+  */
     return (TLIB_ERROR);
 }
 
 
 int tlib_yield(int wantTid)
 {
-	return (TLIB_ERROR);
+    //root pointerı sağa kaydır.
+  	return (TLIB_ERROR);
 }
 
 
 int tlib_delete_thread(int tid)
 {
+    /*
+        stackten stubı sil.
+        queuedan node'u sil
+
+    */
     return (TLIB_ERROR);
 }
-
